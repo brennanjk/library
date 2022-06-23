e1ec389f7a5d8d01ef03c4284615af520a37d441
Brennan Kilpatrick <brennankilpatrick@gmail.com>
2022-06-23 16:51:09 -0700

    Add HTML basic structure. Add js booklibrary array and book constructor objects

---
+++ app.js
@@ -0,0 +1,8 @@
+let bookLibrary = [];
+
+function book(title, author, pageCount, haveRead) {
+    this.title = title;
+    this.author = author;
+    this.pageCount = pageCount
+    this.haveRead = haveRead
+}
