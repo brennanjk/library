4ebd105821a13de4cd4d5af8d7df62b1bdd12896
Brennan Kilpatrick <brennankilpatrick@gmail.com>
2022-06-24 10:07:45 -0700

    Create testbook to test book constructor function. Create addBooks function to test adding books to bookLibrary array
    Successfully tested in the DOM

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 let bookLibrary = [];
 
+/* book constructor */
 function book(title, author, pageCount, haveRead) {
     this.title = title;
     this.author = author;
     this.pageCount = pageCount
     this.haveRead = haveRead
+}
+
+/* test book */
+
+let testBook = new book('Fear and Loathing in Las Vegas','Hunter S. Thompson', 204, true )
+
+/* Add book to bookLibrary */
+
+function addBooks(book) {
+    bookLibrary.push(book);
 }
